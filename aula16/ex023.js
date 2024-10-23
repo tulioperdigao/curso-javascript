b3b0295963574eac57f126472362a87e03e2f8a4
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-23 15:41:20 -0300

    aula 16

---
+++ aula16/ex023.js
@@ -0,0 +1,10 @@
+function parImpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = parImpar(223)
+console.log(res)
---
+++ aula16/ex027.js
@@ -0,0 +1,10 @@
+// RECURSIVIDADE
+function fatorial(num) {
+    if (num == 1) {
+        return 1
+    } else {
+        return num * fatorial(num - 1)
+    }
+}
+
+console.log(fatorial(5))
